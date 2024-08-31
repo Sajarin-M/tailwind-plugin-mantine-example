afbfb23dd8692fef2721a47a19d73b0c77af0af5
Sajarin M <sajarinm@yahoo.com>
2024-08-31 11:24:39 +0530

    add example files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,9 @@
 import type { Config } from "tailwindcss";
+import pluginMantine from "@devoss/tailwind-plugin-mantine";
+import { theme } from "./src/lib/mantine-theme";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  plugins: [pluginMantine(theme)],
 };
 export default config;
